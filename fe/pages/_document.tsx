de4d1a561f692799094ec21189ca17ed47f7b8ac
IIAbinashMishraII <99abinash.mishra@gmail.com>
2024-07-03 09:04:39 +0530

    fe update

---
+++ fe/pages/_document.tsx
@@ -0,0 +1,24 @@
+import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+export default function Document() {
+  return (
+    <Html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="author" content="Abinash Mishra" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css"
+          integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
