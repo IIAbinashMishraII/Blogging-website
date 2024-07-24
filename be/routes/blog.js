8d964e684b5b6de5d74c858368259042ca2a66c1
IIAbinashMishraII <99abinash.mishra@gmail.com>
2024-07-24 23:00:25 +0530

    update

--- be/routes/blog.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { create } = require("../controllers/blog");
-const router = express.Router();
-const { requireSignin, adminMiddleware } = require("../controllers/auth");
-
-router.post("/blog", requireSignin, adminMiddleware, create);
-
-module.exports = router;
--- fe/config.js
+++
@@ -1,7 +0,0 @@
-import getConfig from "next/config.js";
-const {publicRuntimeConfig} = getConfig();
-
-export const API = publicRuntimeConfig.PRODUCTION
-  ? "https://blogged.com"
-  : "http://localhost:8000/api";
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
